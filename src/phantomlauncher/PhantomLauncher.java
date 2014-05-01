2bd64c833b1a0271ae09a21453037f6dbe7f06b3
Alexander <Alexander@Bamsen>
2014-05-01 17:42:06 +0200

    Closing / minimizing / switching windows now posible. Now undecorated.

--- src/phantomlauncher/PhantomLauncher.java
+++ src/phantomlauncher/PhantomLauncher.java
@@ -7,10 +7,9 @@ package phantomlauncher;
 
 import javafx.application.Application;
 import static javafx.application.Application.launch;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 /**
  *
@@ -24,22 +23,24 @@ public class PhantomLauncher extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        
-                // We need a handler that keeps track of the different "screens" we 
+
+        // We need a handler that keeps track of the different "screens" we 
         // want to switch between. We also need to add the different
         // "screens" to that handler.
-        
         ScreenController handler = new ScreenController(stage);
         handler.loadScreen("Login", "Login.fxml");
         handler.loadScreen("SignUp", "SignUp.fxml");
         handler.loadScreen("Profile", "Profile.fxml");
-        
+
         handler.setScreen("Login"); // Start with this screen.
-        
+
         Scene scene = new Scene(handler);
         stage.setScene(scene);
+        stage.initStyle(StageStyle.UNDECORATED);
+        stage.initStyle(StageStyle.TRANSPARENT);
         stage.show();
         stage.setTitle("Phantom Launcher");
         stage.setResizable(false);
+
     }
 }
